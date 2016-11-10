41b6caf71dac7ac3b03e8bdc4a0fff4cde572f28
Luka <Luka@DESKTOP-KR2PEFL>
2016-11-10 14:39:08 +0100

    Novo

--- Dobre misli/Default.aspx.cs
+++ Dobre misli/Default.aspx.cs
@@ -7,16 +7,20 @@ using System.Web.UI.WebControls;
 
 namespace Dobre_misli
 {
+    
     public partial class Default : System.Web.UI.Page
     {
+        Label bla = new Label();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+           
         }
 
         protected void dodajMisel_Click(object sender, EventArgs e)
         {
-                    
+            
+            bla.Text = bla.Text+"\n"+misel.Text;
+            misli.Controls.Add(bla);
         }
     }
     
